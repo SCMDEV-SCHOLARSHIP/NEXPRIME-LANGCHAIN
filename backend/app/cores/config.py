07f82dac9fcfe7e1bb54f1f30466b1d94607ff85
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-02-26 15:03:02 +0900

    Change config structures
    설정 변수 제한 강화: property 추가, API KEY 출력 마스킹 & 출력 제한, frozen 선언
    환경 변수 확장, 이에 따른 변수 추가

--- backend/app/cores/config.py
+++ backend/app/cores/config.py
@@ -1,57 +1,84 @@
+from pydantic import SecretStr, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .constants import BasePath
 from .utils import as_posix
 
 
-class Settings(BaseSettings):
+class SecretSettings(BaseSettings):
     model_config = SettingsConfigDict(
         secrets_dir=BasePath.SECRETS,
     )
-    OPENAI_API_KEY: str
+    OPENAI_API_KEY: SecretStr = Field(frozen=True)
 
 
-class BaseEnvirons(Settings):
-    env_root: str = BasePath.ENVS
+class EnvironInfo(BaseSettings, extra="forbid"):
+    env_root: str = Field(default=BasePath.ENVS, frozen=True)
     project_name: str = "KMG"
+    host: str = Field(frozen=True)
+    port: int = Field(frozen=True)
+    url: str = Field(frozen=True)
 
 
-class BaseVectorStore(BaseSettings):
-    env_root: str = BasePath.ENVS
-    host: str = "localhost"
-    port: int = 8000
-    url: str = ":".join([host, str(port)])
+class VectorStoreInfo(BaseSettings, extra="forbid"):
+    engine: str
+    env_root: str = Field(default=BasePath.ENVS, frozen=True)
+    host: str = Field(frozen=True)
+    port: int = Field(frozen=True)
+    url: str = Field(frozen=True)
 
 
-class DevQdrantSettings(BaseVectorStore):
+class DevQdrantSettings(VectorStoreInfo, extra="forbid"):
     model_config = SettingsConfigDict(
-        env_file=as_posix(BaseVectorStore().env_root, "dev_qdrant.env"),
+        env_file=as_posix(
+            VectorStoreInfo.model_fields["env_root"].default, "dev_qdrant.env"
+        ),
         env_prefix="QDRANT_",
     )
+    engine: str = Field(default="qdrant", frozen=True)
 
 
-class DevSettings(BaseEnvirons):
+class DevSettings(EnvironInfo, extra="forbid"):
     model_config = SettingsConfigDict(
-        env_file=as_posix(BaseEnvirons().env_root, "dev.env")
+        env_file=as_posix(EnvironInfo.model_fields["env_root"].default, "dev.env")
     )
 
 
 class RunSettings:
     def __init__(self, mode: str = "dev", vectorstore: str = "qdrant") -> None:
-        self.mode = mode
-        self.environ = self.get_environ()
-        self.vectorstore = self.get_vectorstore(vectorstore)
+        self._mode = mode
+        self._environ = self._get_environ()
+        self._vectorstore = self._get_vectorstore(vectorstore)
 
-    def get_environ(self) -> BaseEnvirons:
+    @property
+    def mode(self) -> str:
+        return self._mode
+
+    @property
+    def environ(self) -> EnvironInfo:
+        return self._environ
+
+    @property
+    def vectorstore(self) -> VectorStoreInfo:
+        return self._vectorstore
+
+    @property
+    def OPENAI_API_KEY(self) -> str:
+        return SecretSettings().OPENAI_API_KEY.get_secret_value()
+
+    def _get_environ(self) -> EnvironInfo:
         if self.mode == "dev":
             return DevSettings()
         raise Exception("Wrong Options")
 
-    def get_vectorstore(self, vectorestore: str) -> BaseVectorStore:
+    def _get_vectorstore(self, vectorestore: str) -> VectorStoreInfo:
         if self.mode == "dev":
             if vectorestore == "qdrant":
                 return DevQdrantSettings()
-        raise Exception("Wrong Options")
+            else:
+                raise Exception("Wrong Options")
+        else:
+            raise Exception("Wrong Options")
 
     def print_dump(self) -> None:
         self._recurse_dump(self.__dict__)
