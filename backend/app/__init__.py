3d587192637122d8a113dba7bb88b68e80a43f74
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-02-29 14:23:42 +0900

    Add exceptions into app
    작성한 exception-handler pair를 추가하는 코드 작성

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,16 @@
+from typing import Generator
 from fastapi import FastAPI
 
 from app.cores.config import settings
 import app.routers as routers
+from app.cores.exceptions import EXC_HDLRs
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(title=settings.environ.project_name)
+    app = FastAPI(
+        title=settings.environ.project_name,
+        exception_handlers=EXC_HDLRs if EXC_HDLRs else None,
+    )
     app.include_router(router=routers.collection_apis, tags=["Collection"])
     app.include_router(router=routers.embedding_apis, tags=["Embedding"])
     return app
