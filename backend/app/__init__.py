49f0782b3987af24748c38ccc288198b921c49e9
hsolemio-lee <hslyjy@gmail.com>
2024-02-29 18:04:49 +0900

    [SCMDEV-52] PostgeSQL 연계 * DB Config 추가 * config.py EnvironInfo에 DB 관련 정보 추가 * SDSEmbeddings.py => sds_embeddings.py (네이밍 룰 위반..) * user.py, test_dbms_router.py, test_schema.py는 테스트를 위한 용도로 추후 삭제 필요 * requirements.txt 업데이트

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,4 +8,7 @@ def create_app() -> FastAPI:
     app = FastAPI(title=settings.environ.project_name)
     app.include_router(router=routers.collection_apis, tags=["Collection"])
     app.include_router(router=routers.embedding_apis, tags=["Embedding"])
+
+    # TODO: Remove after test
+    app.include_router(router=routers.test_apis, tags=["Test"])
     return app
