1abf617fb5b816587262cbd8fa698d86673beb0f
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-03-07 14:36:13 +0900

    Add common types

--- backend/app/cores/common_types.py
+++ backend/app/cores/common_types.py
@@ -3,6 +3,8 @@ from langchain.document_loaders.base import BaseLoader
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.messages import BaseMessage
+from langchain_core.language_models import BaseLanguageModel
+from langchain.vectorstores import VectorStore
 from qdrant_client import QdrantClient
 import qdrant_client.http.models as qtypes
 
