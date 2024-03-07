3696a9c52de9a32d604255584ab853580abb1906
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-03-07 14:44:01 +0900

    Build retrieval service for extending
    확장 가능한 검색 서비스 구현
    1. di_container.py
    * builder 패턴 적용, 기존의 dependencies.py에 있던 내용 복사
    * 서비스 빌더 코드 추가
    
    2. retrieval_router.py
    * 서비스 생성 & retrieve
    
    3. retrival_service.py
    * 기존 서비스 인터페이스화, 프롬프트는 하위 클래스들이 각자 구현
    * 각자의 검색 방법 구현 가능

--- backend/app/cores/di_container.py
+++ backend/app/cores/di_container.py
@@ -1,10 +1,73 @@
+from __future__ import annotations
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Configuration, Singleton
+from dependency_injector.providers import Configuration, Singleton, Self
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+
+from app.cores.config import settings
+import app.cores.common_types as types
 from app.services.user_service import UserService
+import app.services.retrieval_service as ret_serv
 from app.repository import UserCrud
+from app.database import ExtendedQdrant
+from app.cores.constants import SupportedModels, SupportedVectorStores
+from app.models.sds_embeddings import SDSEmbedding
+
+
+# TODO: dependencies로 옮기기 + builder 분할(embedding 등)
+class RetrievalMaker:  # TODO: director로 역할 변환
+    def __init__(self, container) -> None:
+        self.container = container  # TODO: 컨테이너에서 setting 받아오기
+
+    def build_retrieval_service(
+        self,
+        llm_model_name: str,
+        embedding_model_name: str,
+        vectorstore_params: dict,
+        choice: str = "openai-test",
+    ) -> ret_serv.RetrievalService:
+        llm = self.make_llm(llm_model_name)
+        embedding = self.make_embedding(embedding_model_name)
+        vectorstore = self.make_vectorstore(vectorstore_params)
+        vectorstore._embeddings = embedding
+        if choice == "openai-test":
+            return ret_serv.OpenAIRetrievalService(vectorstore=vectorstore, llm=llm)
+        else:
+            raise Exception("Value not found")
+
+    def make_vectorstore(self, params: dict) -> types.VectorStore:
+        vs_name = settings.vectorstore.engine
+        if vs_name == SupportedVectorStores.QDRANT:
+            return ExtendedQdrant(**params)
+        else:
+            raise Exception("Value not found")
+
+    def make_embedding(self, model_name: str) -> types.Embeddings:
+        engine = SupportedModels.EMBEDDING.get(model_name, None)
+        if engine == "openai":
+            return OpenAIEmbeddings(
+                model=model_name,
+                openai_api_key=settings.OPENAI_API_KEY,
+            )
+        elif engine == "sds-embed":
+            return SDSEmbedding()
+        else:
+            raise Exception("Value not found")
+
+    def make_llm(self, model_name: str) -> types.BaseLanguageModel:
+        engine = SupportedModels.LLM.get(model_name, None)
+        if engine == "openai":
+            return ChatOpenAI(
+                model=model_name,
+                temperature=0,
+                api_key=settings.OPENAI_API_KEY,
+            )
+        else:
+            raise Exception("Value not found")
 
 
 class DiContainer(DeclarativeContainer):
+    __self__ = Self()
     config = Configuration()
 
     # repositories
@@ -12,3 +75,6 @@ class DiContainer(DeclarativeContainer):
 
     # services
     user_service = Singleton(UserService, user_crud=user_crud)
+
+    # builder / director
+    retrieval_maker = Singleton(RetrievalMaker, __self__)
