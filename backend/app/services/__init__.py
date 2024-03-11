fa3dfa26a6525ea42da470908a6ab1b891e3f57a
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-03-11 12:18:06 +0900

    Rebuild retrieval structures
    * 서비스를 DiContainer에서 주입받도록 변경

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,4 @@
+from .embedding_service import EmbeddingService
+from .collection_service import CollectionService
+from .retrieval_service import RetrievalService, OpenAIRetrievalService
+from .user_service import UserService
