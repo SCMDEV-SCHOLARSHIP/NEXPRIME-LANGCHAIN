a9066e161c0bdb62b64b4fcc1f544a5edf880b21
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-03-22 10:42:29 +0900

    Add auth router
    * 새 토큰 발급 & 갱신 API 추가
    * 신규 발급 시 인증 코드는, login 기능이 없어 임시로 test만 받음

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -2,3 +2,4 @@ from .collection_router import router as collection_apis
 from .embedding_router import router as embedding_apis
 from .retrieval_router import router as retrieval_apis
 from .user_router import router as user_apis
+from .auth_router import router as auth_apis
