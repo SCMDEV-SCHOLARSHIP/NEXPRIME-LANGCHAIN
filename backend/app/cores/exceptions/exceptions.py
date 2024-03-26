76b478f623008494b2a724864b308353ba4b9fe9
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-03-26 09:47:26 +0900

    Build exception interface
    * Exception에 SDSException 추가, 양식 통일
    * Handler 양식 통일

--- backend/app/cores/exceptions/exceptions.py
+++ backend/app/cores/exceptions/exceptions.py
@@ -1,30 +1,51 @@
 from fastapi import status
-from fastapi.exceptions import HTTPException
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from app.cores.exceptions.error_code import ErrorCode
 
 
-class InvalidRequestException(StarletteHTTPException):
+class SDSHTTPException(StarletteHTTPException):
     def __init__(
-        self, value: object, error_code: ErrorCode, detail: str | None = None
+        self,
+        value: object,
+        error_code: ErrorCode,
+        status_code: int,
+        detail: str | None = None,
     ) -> None:
         self.value = value
         self.error_code = error_code
         if detail == None:
-            detail = f"{ErrorCode.BAD_REQUEST.value[0]} : {ErrorCode.BAD_REQUEST.value[1]} - {value}"
+            detail = f"{error_code.value[0]} : {error_code.value[1]} - {value}"
 
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+        super().__init__(status_code=status_code, detail=detail)
 
 
-class ExternalServiceException(StarletteHTTPException):
+class InvalidRequestException(SDSHTTPException):
     def __init__(
         self, value: object, error_code: ErrorCode, detail: str | None = None
     ) -> None:
-        self.value = value
-        self.error_code = error_code
-        if detail == None:
-            detail = f"{ErrorCode.EXTERNAL_SERVICE_ERROR.value[0]} : {ErrorCode.EXTERNAL_SERVICE_ERROR.value[1]} - {value}"
+        super().__init__(value, error_code, status.HTTP_400_BAD_REQUEST, detail)
 
+
+class ForbiddenAccessException(SDSHTTPException):
+    def __init__(
+        self, value: object, error_code: ErrorCode, detail: str | None = None
+    ) -> None:
+        super().__init__(value, error_code, status.HTTP_403_FORBIDDEN, detail)
+
+
+class InternalServerException(SDSHTTPException):
+    def __init__(
+        self, value: object, error_code: ErrorCode, detail: str | None = None
+    ) -> None:
         super().__init__(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail
+            value, error_code, status.HTTP_500_INTERNAL_SERVER_ERROR, detail
+        )
+
+
+class ExternalServiceException(SDSHTTPException):
+    def __init__(
+        self, value: object, error_code: ErrorCode, detail: str | None = None
+    ) -> None:
+        super().__init__(
+            value, error_code, status.HTTP_500_INTERNAL_SERVER_ERROR, detail
         )
