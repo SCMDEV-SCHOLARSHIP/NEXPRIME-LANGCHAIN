7a7d0a766563ebaf073c3fc0cc544d3c8162034d
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-04-17 15:22:37 +0900

    Add a util to get payload

--- backend/app/cores/common_types.py
+++ backend/app/cores/common_types.py
@@ -2,12 +2,16 @@ from typing import TypeAlias
 
 from langchain.document_loaders.base import BaseLoader
 from langchain.text_splitter import TextSplitter
+from langchain.vectorstores import VectorStore
+
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.messages import BaseMessage
 from langchain_core.language_models import BaseLanguageModel
-from langchain.vectorstores import VectorStore
+from langchain_core.runnables import Runnable
+from langchain_core.prompts import BasePromptTemplate
 from langchain_core.runnables.utils import AddableDict
+from langchain_core.chat_history import BaseChatMessageHistory
 
 from qdrant_client import QdrantClient
 import qdrant_client.http.models as qtypes
