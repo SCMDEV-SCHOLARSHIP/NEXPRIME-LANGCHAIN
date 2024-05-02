02fb26c129ea057892262ee09f7113ddb9c267dc
SeungWoo-You <55529141+YouSeungWoo@users.noreply.github.com>
2024-05-02 14:55:45 +0900

    Fix not defined parameter

--- backend/app/schemas/history_schema.py
+++ backend/app/schemas/history_schema.py
@@ -3,5 +3,5 @@ from app.schemas.message_schema import MessageIdentifierRequest
 
 class ReconstructionRequest(MessageIdentifierRequest, extra="forbid"):
     llm_model: str
-    reconstruction_type: str = "buffer_window"
+    memory_type: str = "buffer_window"
     cut_off: int = 6
